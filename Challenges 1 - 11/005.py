fc2b01405f6ce3c0369bfd86b4ffbad7d3e71bbb
Nnamdi10 <116636161+Nnamdi10@users.noreply.github.com>
2025-01-21 16:24:50 +0000

    Create 005.py
    Ask the user to enter three numbers. Add together the first two numbers and then multiply this total by the third. Display the answer as The answer is [answer].

---
+++ Challenges 1 - 11/005.py
@@ -0,0 +1,12 @@
+#Ask user to input 3 numbers
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+num3=int(input("Enter third number: "))
+
+#Sum the first 2 numbers
+add_n=num1 + num2
+
+#Multiply sum and 3rd number
+total=add_n * num3
+
+print("The answer is", total)
