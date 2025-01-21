7078a58bf13d6df3777fb7cfba76f94545fe3292
Nnamdi10 <116636161+Nnamdi10@users.noreply.github.com>
2025-01-21 18:01:48 +0000

    Create 011.py
    Task the user to enter a number over 100 and then enter a number under 10 and tell them how many times the smaller number goes into the larger number in a user-friendly format.

---
+++ Challenges 1 - 11/011.py
@@ -0,0 +1,10 @@
+#input number over 100
+bn=int(input("Enter a number over 100: "))
+
+#input number below 10
+sn=int(input("Enter a number below 10: "))
+
+#how many times sn goes into bn
+divs=bn/sn
+
+print("The smaller number goes into the larger number", divs, "times.")
