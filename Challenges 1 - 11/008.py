1d2014256bb348a6ceb1fd9881a73242370ead0c
Nnamdi10 <116636161+Nnamdi10@users.noreply.github.com>
2025-01-21 17:51:44 +0000

    Create 008.py
    Ask for the total price of the bill, then ask how many diners there are. Divide the total bill by the number of diners and show how much each person must pay.

---
+++ Challenges 1 - 11/008.py
@@ -0,0 +1,10 @@
+#total price of bill
+tpob=int(input("Input the total price of the bill?: "))
+
+#number of diners
+nod=int(input("Input the number of diners?: "))
+
+#divide the total bill by the number of diners
+toco=tpob / nod
+
+print("Each person must pay", toco)
